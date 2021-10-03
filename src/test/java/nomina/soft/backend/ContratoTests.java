a9834d965533b5d0a193b38a4e4e845b8c733ca2
Josemaro <josemawo17@gmail.com>
2021-10-03 16:05:08 -0500

    fechasValidas

--- src/test/java/nomina/soft/backend/ContratoTests.java
+++ src/test/java/nomina/soft/backend/ContratoTests.java
@@ -1,8 +1,10 @@
 package nomina.soft.backend;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Date;
@@ -18,6 +20,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import nomina.soft.backend.constant.ContratoImplConstant;
 import nomina.soft.backend.exception.domain.ContratoNotValidException;
 import nomina.soft.backend.models.ContratoModel;
 import nomina.soft.backend.services.impl.ContratoServiceImpl;
@@ -28,7 +31,7 @@ public class ContratoTests {
 
 	//Regla 1
 	@Test 
-	void validarVigenciaTestCasoValido() throws ParseException {
+	void validarVigenciaTest1() throws ParseException {
 		ContratoModel contrato = new ContratoModel();
 		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 		Date fechaInicio = formatter.parse("3/10/2021");
@@ -41,7 +44,7 @@ public class ContratoTests {
 	}
 	
 	@Test
-	void validarVigenciaTestEstaCancelado() throws ParseException {
+	void validarVigenciaTest2() throws ParseException {
 		
 		ContratoModel contrato = new ContratoModel();
 		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
@@ -55,7 +58,7 @@ public class ContratoTests {
 	}
 
 	@Test
-	void validarVigenciaTestFechaNoValida() throws ParseException {
+	void validarVigenciaTest3() throws ParseException {
 		
 		ContratoModel contrato = new ContratoModel();
 		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
@@ -66,6 +69,63 @@ public class ContratoTests {
 		contrato.setFechaFin(fechaFin);
 		contrato.setEstaCancelado(true);
 		assertFalse(contrato.vigenciaValida(contrato));
+	}
+
+	@Test
+	void fechasValidasTest1() throws ParseException, ContratoNotValidException {
+		ContratoImplConstant exceptionMsg = new ContratoImplConstant();
+		ContratoModel contrato = new ContratoModel();
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		Date fechaInicio = formatter.parse("3/9/2021");
+		Date fechaFin = formatter.parse("3/12/2021");
+		contrato.setFechaInicio(fechaInicio);
+		contrato.setFechaFin(fechaFin);
+
+		//contrato.fechasValidas(fechaInicio,fechaFin)
+		String resultado = new String();
+		try {
+			assertTrue(contrato.fechasValidas(fechaInicio,fechaFin));
+		} catch (Exception e) {
+			resultado = e.getMessage();
+			assertEquals(exceptionMsg.FECHA_INICIO_NOT_VALID,resultado);
+		}
+		
+	}
+
+	@Test
+	void fechasValidasTest2() throws ParseException, ContratoNotValidException {
+		ContratoImplConstant exceptionMsg = new ContratoImplConstant();
+		ContratoModel contrato = new ContratoModel();
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		Date fechaInicio = formatter.parse("4/12/2021");
+		Date fechaFin = formatter.parse("5/12/2021");
+		contrato.setFechaInicio(fechaInicio);
+		contrato.setFechaFin(fechaFin);
+
+		//contrato.fechasValidas(fechaInicio,fechaFin)
+		String resultado = new String();
+		try {
+			assertTrue(contrato.fechasValidas(fechaInicio,fechaFin));
+		} catch (Exception e) {
+			resultado = e.getMessage();
+			assertEquals(exceptionMsg.FECHA_FIN_NOT_VALID,resultado);
+		}
+	}
+
+	@Test
+	void fechasValidasTest3() throws ParseException, ContratoNotValidException {
+		ContratoImplConstant exceptionMsg = new ContratoImplConstant();
+		ContratoModel contrato = new ContratoModel();
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		Date fechaInicio = formatter.parse("15/10/2021");
+		Date fechaFin = formatter.parse("15/05/2022");
+		contrato.setFechaInicio(fechaInicio);
+		contrato.setFechaFin(fechaFin);
+
+		//contrato.fechasValidas(fechaInicio,fechaFin)
+		String resultado = new String();
+		assertTrue(contrato.fechasValidas(fechaInicio,fechaFin));
+
 	}
 
 }
